133a852a6d920217f83caca0f8e16b7f62b7f065
mastter <123456@qq.com>
2018-07-14 14:58:28 +0800

    第一次提交

--- src/org/westos/ServletDemo.java
+++ src/org/westos/ServletDemo.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class ServletDemo extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("hehe");
+        System.out.println("hehe");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
